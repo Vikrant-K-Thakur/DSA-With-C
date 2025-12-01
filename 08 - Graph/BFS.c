30546ec201426a96b118075da53823806145966e
Vikrant <vikrantkthakur123@gmail.com>
2025-12-01 18:44:01 +0530

    BFS and DFS

--- 08 - Graph/BFS.c
+++ 08 - Graph/BFS.c
@@ -1,109 +1,70 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-struct queue
-{
-    int size;
-    int f;
-    int r;
-    int *arr;
-};
+int adj[10][10], visited[10], queue[10]; // adjacency matrix, visited array, queue
+int n, front = -1, rear = -1;            // n = number of nodes, queue pointers
 
-int isFull(struct queue *q)
+void bfs(int start)
 {
-    if (q->r == q->size - 1)
-    {
-        return 1;
-    }
-    return 0;
-}
+    int i;
 
-int isEmpty(struct queue *q)
-{
-    if (q->f == q->r)
-    {
-        return 1;
-    }
-    return 0;
-}
+    visited[start] = 1;    // mark starting node as visited. For example: visited[1] = 1
+    queue[++rear] = start; // insert start node into queue. rear becomes 0, queue[0] = 1
+    front++;               // front becomes 0 (first element of queue)
 
-void enqueue(struct queue *q, int val)
-{
-    if (isFull(q))
+    // BFS processing loop (runs until queue becomes empty)
+    while (front <= rear)
     {
-        printf("Queue is overflow");
-    }
-    else
-    {
-        q->r = q->r + 1;
-        q->arr[q->r] = val;
-    }
-}
+        int node = queue[front++]; // remove element from queue. First time: node = 1, front = 1
+        printf("%d ", node);       // print the node in BFS order
 
-int dequeue(struct queue *q)
-{
-    int a = -1;
-    if (isEmpty(q))
-    {
-        printf("Queue is underflow");
+        // Explore all neighbors of current 'node'
+        for (i = 1; i <= n; i++)
+        {
+            // Check if there is an edge AND the neighbor is not visited
+            // Example: for node=1, adj[1][2] = 1 → so 2 is neighbor
+            if (adj[node][i] == 1 && visited[i] == 0)
+            {
+                queue[++rear] = i; // add neighbor to queue (e.g., queue[1] = 2, queue[2] = 3)
+                visited[i] = 1;    // mark neighbor as visited
+            }
+        }
     }
-    else
-    {
-        q->f++;
-        a = q->arr[q->f];
-    }
-    return a;
 }
 
 int main()
 {
-    struct queue q;
-    q.size = 400;
-    q.f = q.r = 0;
-    q.arr = (int *)malloc((q.size) * sizeof(int));
-
-    int u;
-    int i = 1;
-    int visited[7] = {0, 0, 0, 0, 0, 0, 0};
 
     /*
-
-                0
-            /   |   \
-           1----2----3
-                 \   /
-                  \ /
-                   4
-                 /   \
-                5     6
-
+        Initial BST:
+              1
+             / \
+            3   2
+             \  /
+               4
     */
-    int a[7][7] = {
-        {0, 1, 1, 1, 0, 0, 0},
-        {1, 0, 1, 0, 0, 0, 0},
-        {1, 1, 0, 1, 1, 0, 0},
-        {1, 0, 1, 0, 1, 0, 0},
-        {0, 0, 1, 1, 0, 1, 1},
-        {0, 0, 0, 0, 1, 0, 0},
-        {0, 0, 0, 0, 1, 0, 0},
-    };
 
-    printf("%d ", i);
-    visited[i] = 1;
-    enqueue(&q, i);
-    while (!isEmpty(&q))
-    {
-        int node = dequeue(&q);
-        for (int j = 0; j < 7; j++)
-        {
-            if (a[node][j] == 1 && visited[j] == 0)
-            {
-                printf("%d ", j);
-                visited[j] = 1;
-                enqueue(&q, j);
-            }
-        }
-    }
-    printf("\n");
+
+    int i, j, start;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n); // for our example, user enters 4
+
+    printf("Enter adjacency matrix:\n");
+
+    // read adjacency matrix from user
+    // for example user enters:
+    // 0 1 1 0
+    // 1 0 0 1
+    // 1 0 0 1
+    // 0 1 1 0
+    for (i = 1; i <= n; i++)
+        for (j = 1; j <= n; j++)
+            scanf("%d", &adj[i][j]);
+
+    printf("Enter starting node: ");
+    scanf("%d", &start); // user enters 1
+
+    bfs(start); // call BFS starting from node 1
+
     return 0;
 }
--- 08 - Graph/DFS.c
+++ 08 - Graph/DFS.c
@@ -1,39 +1,62 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int visited[7] = {0, 0, 0, 0, 0, 0, 0};
-int a[7][7] = {
-    {0, 1, 1, 1, 0, 0, 0},
-    {1, 0, 1, 0, 0, 0, 0},
-    {1, 1, 0, 1, 1, 0, 0},
-    {1, 0, 1, 0, 1, 0, 0},
-    {0, 0, 1, 1, 0, 1, 1},
-    {0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 1, 0, 0},
-};
+int adj[10][10], visited[10], stack[10];
+int top = -1, n;
 
-void DFS(int i)
+/*
+    Initial BST:
+          1
+         / \
+        3   2
+         \  /
+          4
+
+stack = 
+visited = 1 1 1 1
+print = 1 3 4 2
+
+*/
+
+
+void dfs(int start)
 {
-    printf("%d ", i);
-    visited[i] = 1;
-    for (int j = 0; j < 7; j++)
-    {
-        if (a[i][j] == 1 && !visited[j])
+    stack[++top] = start; // push start node
+    visited[start] = 1;   // mark start visited
+
+    while (top != -1)
+    { // while stack not empty
+
+        int node = stack[top--]; // pop element from stack
+        printf("%d ", node);     // print the node
+
+        // check all neighbors (in reverse order to match logic)
+        for (int i = n; i >= 1; i--)
         {
-            DFS(j);
+            if (adj[node][i] == 1 && visited[i] == 0)
+            {
+                stack[++top] = i; // push neighbor
+                visited[i] = 1;   // mark visited
+            }
         }
-        
     }
-    
-    
 }
 
 int main()
 {
-    DFS(0);
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            scanf("%d", &adj[i][j]);
+
+    int start;
+    printf("Enter starting node: ");
+    scanf("%d", &start);
+
+    dfs(start);
 
     return 0;
 }
-
-
-
